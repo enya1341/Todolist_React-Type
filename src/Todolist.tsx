e8311e394c42a3d56b7a06afed83334644b46740
enya1341 <ryuusei.y@icloud.com>
2023-01-26 20:28:18 +0900

    TODOリスト。最低限完成2

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -2,8 +2,14 @@ import "./Todolist.css";
 import Todo from "./todo";
 import React from "react";
 
+interface todoObj {
+  id: number;
+  name: string;
+  completed: boolean;
+}
+
 export const Todolist: React.FC<any> = ({ todos, toggleTodo }) => {
-  return todos.map((todo: any) => (
+  return todos.map((todo: todoObj) => (
     <Todo todo={todo} toggleTodo={toggleTodo} key={todo.id} />
   ));
 };
